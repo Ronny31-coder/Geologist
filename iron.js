76f2c5a63179e13481440fc8e69dbadcc2015b3c
Ronny31-coder <74668023+Ronny31-coder@users.noreply.github.com>
2021-03-25 20:31:55 +0530

    Add files via upload

---
+++ iron.js
@@ -0,0 +1,9 @@
+class Iron{
+    constructor(x,y,width,height){
+        var options={
+            'restitution': 0.8,
+            'friction': 3,
+            'density': 30,
+        }
+    }
+}
---
+++ sketch.js
@@ -0,0 +1,33 @@
+const Engine = Matter.Engine;
+const World= Matter.World;
+const Bodies = Matter.Bodies;
+
+var engine, world;
+var hammer;
+
+
+function setup(){
+    var canvas = createCanvas(1200,600);
+    engine = Engine.create();
+    world = engine.world;
+
+    plane = new Plane(600,height,1200,20);
+    hammer = new Hammer(10,100);
+
+    rubbr = new Rubber(220,100,50);
+    ston = new Stone(120,40,50,50);
+
+}
+
+function draw(){
+    background("lightBlue");
+    Engine.update(engine);
+
+
+    plane.display();
+    hammer.display();
+
+    rubbr.display();
+    ston.display();
+ 
+}
